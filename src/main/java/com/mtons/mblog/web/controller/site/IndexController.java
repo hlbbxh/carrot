6a85e3e6c6363af35336dd86bf95a2f49ea04b25
yandihu <1394105227@qq.com>
2019-04-13 19:28:32 +0800

    index的action的处理

--- src/main/java/com/mtons/mblog/web/controller/site/IndexController.java
+++ src/main/java/com/mtons/mblog/web/controller/site/IndexController.java
@@ -25,13 +25,20 @@ import com.mtons.mblog.web.controller.BaseController;
  */
 @Controller
 public class IndexController extends BaseController{
-	
+	/**
+	 * 这是 网站的住入口 当地址输入/ 和   /index的时候自动返回主页
+	 * @param model
+	 * @param request
+	 * @return
+	 */
 	@RequestMapping(value= {"/", "/index"})
 	public String root(ModelMap model, HttpServletRequest request) {
 		String order = ServletRequestUtils.getStringParameter(request, "order", Consts.order.NEWEST);
 		int pageNo = ServletRequestUtils.getIntParameter(request, "pageNo", 1);
 		model.put("order", order);
 		model.put("pageNo", pageNo);
+		System.out.println(view(Views.INDEX));
+		//    什么都不配置的时候是返回的这个字符串                 -->     /classic/index
 		return view(Views.INDEX);
 	}
 
