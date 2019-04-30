32fc2cbca025a8f107281138d8d0a5f1adc46a19
xiongjun <junxiong@irissz.com>
2019-04-30 11:49:18 +0800

    添加了拦截器的注释

--- src/main/java/com/mtons/mblog/web/interceptor/BaseInterceptor.java
+++ src/main/java/com/mtons/mblog/web/interceptor/BaseInterceptor.java
@@ -30,25 +30,41 @@ public class BaseInterceptor extends HandlerInterceptorAdapter {
 
 	@Autowired
 	private InterceptorHookManager interceptorHookManager;
-
+	
+	/**
+	 *  在请求处理之前进行调用（Controller方法调用之前）
+	 */
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 		interceptorHookManager.preHandle(request, response, handler);
 		return true;
 	}
+	
+	/**
+	 * 请求处理之后进行调用，但是在视图被渲染之前（Controller方法调用之后）
+	 */
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
 			ModelAndView modelAndView) throws Exception {
 		request.setAttribute("base", request.getContextPath());
 		interceptorHookManager.postHandle(request,response,handler,modelAndView);
 	}
-
+	/**
+	 * 	在整个请求结束之后被调用，也就是在DispatcherServlet 渲染了对应的视图之后执行
+	 * （主要是用于进行资源清理工作）
+	 */
 	@Override
 	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
 		super.afterCompletion(request, response, handler, ex);
 		interceptorHookManager.afterCompletion(request, response, handler, ex);
 	}
-
+	
+	/**
+	 * 相对于HandlerInterceptor，HandlerInterceptorAdapter多了一个实现方法afterConcurrentHandlingStarted()
+	 * 就是这个方法  
+	 * 当Controller中有异步请求方法的时候会触发该方法时，异步请求先支持preHandle、然后执行afterConcurrentHandlingStarted。
+	 * 异步线程完成之后执行preHandle、postHandle、afterCompletion。
+	 */
 	@Override
 	public void afterConcurrentHandlingStarted(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 		super.afterConcurrentHandlingStarted(request, response, handler);
